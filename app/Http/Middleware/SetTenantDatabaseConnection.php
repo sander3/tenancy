11a65a69cd07bb6525dace303e2d8d009cf4df65
Sander de Vos <sander@tutanota.de>
2019-07-09 01:02:52 +0200

    Get tenant by subdomain/route or input value

---
+++ app/Http/Middleware/SetTenantDatabaseConnection.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Tenant;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class SetTenantDatabaseConnection
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $mode
+     * @return mixed
+     */
+    public function handle(
+        $request,
+        Closure $next,
+        string $mode = 'implicit'
+    ) {
+        // Get tenant by subdomain/route or input value
+        $tenant = $request->route('tenant') ?? $request->input('tenant');
+
+        if ($this->isExplicit($mode) && is_null($tenant)) {
+            $exception = new ModelNotFoundException;
+            $exception->setModel(Tenant::class);
+
+            throw $exception;
+        }
+
+        return $next($request);
+    }
+
+    public function isExplicit(string $mode)
+    {
+        return $mode === 'explicit';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,18 @@
 */
 
 // Authentication Routes
-Route::post('register', 'Auth\RegisterController@register')->name('auth.register')->middleware('guest');
+Route::middleware('tenant', function () {
+    Route::post('register', 'Auth\RegisterController@register')->name('auth.register')->middleware('guest');
 
-Route::post('email', 'Auth\LinkController@sendMagicLinkEmail')->name('auth.email')->middleware(['guest', 'throttle:5,5']);
+    Route::post('email', 'Auth\LinkController@sendMagicLinkEmail')->name('auth.email')->middleware(['guest', 'throttle:5,5']);
 
-Route::post('logout', 'Auth\LoginController@logout')->name('auth.logout')->middleware('auth');
+    Route::post('logout', 'Auth\LoginController@logout')->name('auth.logout')->middleware('auth');
+});
+
+// Tenant Routes
+Route::group([
+    'domain'     => '{tenant}' . config('tenancy.domain'),
+    'middleware' => 'tenant:explicit',
+], function () {
+    //
+});
